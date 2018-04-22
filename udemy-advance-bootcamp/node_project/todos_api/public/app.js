8256d7f8ef7c5c78842ff3ef2fa0e695937acf6f
Naufaldi <naufaldi.rafif@gmail.com>
2018-04-23 02:22:25 +0700

    es2015

--- udemy-advance-bootcamp/node_project/todos_api/public/app.js
+++ udemy-advance-bootcamp/node_project/todos_api/public/app.js
@@ -8,7 +8,12 @@ $(document).ready(function(){
             createTodo();
         }
     });
-    $('.list').on('click', 'span', function(){
+    $('.list').on('click', 'li',function(){
+        //this menuju 'li'
+        updateTodo($(this));
+    })
+    $('.list').on('click', 'span', function(e){
+        e.stopPropagation();
        removeTodo($(this).parent());
     });
 });
@@ -17,13 +22,14 @@ function addTodos(todos){
     //add todos to db
     todos.forEach(function(todo){
         addTodo(todo);
-    });
+    })
 }
 
 function addTodo(todo){
     //add tiodos page here
         var newTodo = $('<li class="task">' +todo.name+' <span> X </span> </li>');
         newTodo.data('id', todo._id);
+        newTodo.data('completed', todo.completed)
         if(todo.completed){
             newTodo.addClass("done");
         }
@@ -53,5 +59,20 @@ function removeTodo(todo){
     })
     .then(function(data){
         todo.remove();
+    })
+}
+
+function updateTodo(todo){
+    var updateUrl = "/api/todos/" + todo.data("id");
+    var isDone = !todo.data("completed");
+    var updateData = {completed: isDone};
+    $.ajax({
+        method: "PUT",
+        url: updateUrl,
+        data: updateData
+    })
+    .then(function(updatedTodo){
+        todo.toggleClass("done");
+        todo.data("completed", isDone);
     })
 }
