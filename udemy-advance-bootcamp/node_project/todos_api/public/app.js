63fdd892ce3ec8bb2bfff5299482dfec8aa0a0cb
Naufaldi <naufaldi.rafif@gmail.com>
2018-04-22 22:52:15 +0700

    finsih delete button

--- udemy-advance-bootcamp/node_project/todos_api/public/app.js
+++ udemy-advance-bootcamp/node_project/todos_api/public/app.js
@@ -7,7 +7,10 @@ $(document).ready(function(){
         if(event.which == 13){
             createTodo();
         }
-    })
+    });
+    $('.list').on('click', 'span', function(){
+       removeTodo($(this).parent());
+    });
 });
 
 function addTodos(todos){
@@ -19,7 +22,8 @@ function addTodos(todos){
 
 function addTodo(todo){
     //add tiodos page here
-        var newTodo = $('<li class="task">' +todo.name+'</li>');
+        var newTodo = $('<li class="task">' +todo.name+' <span> X </span> </li>');
+        newTodo.data('id', todo._id);
         if(todo.completed){
             newTodo.addClass("done");
         }
@@ -31,9 +35,23 @@ function createTodo(){
     var usrInput = $('#todoInput').val();
     $.post('/api/todos', {name: usrInput})
     .then(function(newTodo){
+        $('#todoInput').val('');
         addTodo(newTodo);
     })
     .catch(function(err){
         console.log(err);
+    })
+}
+
+function removeTodo(todo){
+    var clickedId = todo.data('id');
+    var deleteUrl = 'api/todos/' + clickedId;
+
+    $.ajax({
+        method: 'DELETE',
+        url: deleteUrl
+    })
+    .then(function(data){
+        todo.remove();
     })
 }
