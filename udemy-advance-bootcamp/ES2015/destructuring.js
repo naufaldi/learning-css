2bfbcf792bd62a4cf23ad95b95e6b44b703a685b
Naufaldi <naufaldi.rafif@gmail.com>
2018-04-27 00:03:58 +0700

    object destructuring

---
+++ udemy-advance-bootcamp/ES2015/destructuring.js
@@ -0,0 +1,10 @@
+var instructor = {
+    firstName = "Elie",
+    lastName = "Naufald"
+}
+
+var firstName = instructor.firstName;
+var lastName = instructor.lastName;
+
+firstName; // elie
+lastName; // naufald
