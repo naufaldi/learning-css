80793d37957d39690827ee94184583e8e489273b
Naufaldi <naufaldi.rafif@gmail.com>
2018-08-16 21:43:05 +0700

    finish note

--- udemy-advance-bootcamp/notes/app.js
+++ udemy-advance-bootcamp/notes/app.js
@@ -1,16 +1,38 @@
+var input = d3.select("input");
+var preview = d3.select(".preview");
+
+d3.select("#new-note")
+    .on("submit", function() {
+      d3.event.preventDefault();
+      d3.select("#notes")
+        .append("p")
+          .classed("note", true)
+          .text(input.property("value"));
+      input.property("value", "");
+      setPreview("");
+    })
+
 d3.select(".remove")
-.on("click", function(){
+    .on("click", function() {
+      d3.selectAll(".note")
+          .remove();
+    });
+
+d3.select(".lucky")
+  .on("click", function() {
     d3.selectAll(".note")
-    .remove();
+        .style("font-size", function() {
+          return Math.random() * 100 + "px";
+        });
+  });
+
+input.on("input", function() {
+  var val = d3.event.target.value;
+  setPreview(val);
 })
 
-d3.select(".new-note")
-.on("click", function(){
-    d3.event.preventDefault();
-    var input = d3.select('input');
-    d3.select("#notes")
-    .append('p')
-    .classed('note', true)
-    .text(input.property('value'));
-    input.property('value', '');
-});
+function setPreview(val) {
+  preview
+      .classed("hide", val === "")
+      .text(val);
+}
