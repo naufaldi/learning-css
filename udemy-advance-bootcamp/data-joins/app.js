af13595768cd8d12a983d5aa2ef0536efdbad387
Naufaldi <naufaldi.rafif@gmail.com>
2018-08-17 23:48:50 +0700

    add js to data

---
+++ udemy-advance-bootcamp/data-joins/app.js
@@ -0,0 +1,25 @@
+var quotes=[ {
+    quote: "I see dead people.", movie: "The Sixth Sense", year: 1999, rating: "PG-13"
+}
+
+,
+{
+    quote: "May the force be with you.", movie: "Star Wars: Episode IV - A New Hope", year: 1977, rating: "PG"
+}
+
+,
+{
+    quote: "You've got to ask yourself one question: 'Do I feel lucky?' Well, do ya, punk?", movie: "Dirty Harry", year: 1971, rating: "R"
+}
+
+,
+{
+    quote: "You had me at 'hello.'", movie: "Jerry Maguire", year: 1996, rating: "R"
+}
+
+,
+{
+    quote: "Just keep swimming. Just keep swimming. Swimming, swimming, swiming.", movie: "Finding Nemo", year: 2003, rating: "G"
+}
+
+];
