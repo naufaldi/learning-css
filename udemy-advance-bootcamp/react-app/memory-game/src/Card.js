34f1a36a36647574f5cff12d1135edf315907ed2
naufaldi <naufaldi.rafif@gmail.com>
2019-03-07 11:19:27 +0700

    maemory game fix

---
+++ udemy-advance-bootcamp/react-app/memory-game/src/Card.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './Card.css'
+
+
+const Card = (props) => {
+  let style = {};
+  if(props.showing ){
+    style.backgroundColor = props.backgroundColor;
+  }
+  return (
+    <div
+    onClick = {props.onClick}
+    className="card-container"
+    style={style}
+    />
+  )
+}
+
+Card.propTypes = {
+  showing : PropTypes.bool.isRequired,
+  backgroundColor: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+};
+
+export default Card
---
+++ udemy-advance-bootcamp/react-app/memory-game/src/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './Navbar.css'
+
+const Navbar = ({onNewGame}) => (
+  <header>
+    <h2><a href="">Memory Game</a></h2>
+    <nav>
+      <li><a onClick={onNewGame}>New Game</a></li>
+    </nav>
+  </header>
+);
+
+Navbar.propTypes= {
+  onNewGame : PropTypes.func.isRequired
+};
+export default Navbar;
